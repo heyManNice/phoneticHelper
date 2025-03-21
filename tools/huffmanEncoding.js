0b56eaa72d25c65d51fac618d06ea666c87c198f
heyManNice <3031978990@qq.com>
2025-03-22 00:50:00 +0800

    feat:生成音标符号表

--- tools/huffmanEncoding.js
+++ tools/huffmanEncoding.js
@@ -7,8 +7,8 @@
  * |
  * 所有小写字母符号:
  * abcdefghijklmnopqrstuvwxyz
- * 所有英文音标符号:
- * 
+ * 所有英文音标符号:(根据数据文件生成)
+ * supɚˈiʃz:ərfɔʌmbdɪoʊnɑtɒðweθŋkæɜɡlˌahɝvjɛ ʒg() ̃
  */
 
 const fs = require('fs');
@@ -50,20 +50,51 @@ class FileOp{
     /**
      * 读取json文件
      */
-    readJsonFile(path){
-        const filePath = path.resolve(path);
+    readJsonFile(file){
+        const filePath = path.resolve(file);
         const content = fs.readFileSync(filePath, "utf-8");
         const json = JSON.parse(content);
         return json;
     }
+    writeFile(file, content){
+        const filePath = path.resolve(file);
+        fs.writeFileSync(filePath, content);
+        console.log("write:", file);
+        
+    }
     /**
-     * 找出文件中包含的所有音标
+     * 找出文件中包含的所有音标符号
      */
+    collectPhoneticSymbols(json){
+        const symbolSet = new Set();
+        for(const key in json){
+            const us = json[key].vc_phonetic_us;
+            for(const i in us){
+                symbolSet.add(us[i]); 
+            }
+            const uk = json[key].vc_phonetic_uk;
+            for(const i in uk){
+                symbolSet.add(uk[i]);
+            }
+        }
+        const symbolsStr = Array.from(symbolSet).join("");
+        console.log(`length:${symbolsStr.length} [${symbolsStr}]`);
+        return symbolsStr;
+    }
 }
 
-const fo = new FileOp();
-const json = fo.readJsonFile("./data/words-split.json");
-console.log(json);
+
+function test(){
+    const fo = new FileOp();
+    const json = fo.readJsonFile("./data/words-split.json");
+    const symbols = fo.collectPhoneticSymbols(json);
+    fo.writeFile("./data/symbols.txt", symbols);
+}
+
+if(require.main === module){
+    test(); 
+}
+
 
 module.exports = {
     CodeMap,
