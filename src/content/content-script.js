f22093cf50c4feed7f0c40ddb52edb4502bc9d45
heyManNice <3031978990@qq.com>
2025-03-25 02:21:51 +0800

    refactor:重构代码顺带修复bug而且网站匹配的单词范围更广

--- src/content/content-script.js
+++ src/content/content-script.js
@@ -4,24 +4,75 @@ class PhoneticHerper {
     constructor() {
         console.log("[phoneticHelper] Script loaded");
         console.time("[phoneticHelper] Initialization time");
-        this.updateElements();
-        this.addEventListener(this.elements);
+        const textNodes = this.getAllTextNodes(document.body);
+        this.wrapWords(textNodes);
+        const spanList = document.querySelectorAll(".ph-span");
+        this.bindEvents(spanList);
         console.timeEnd("[phoneticHelper] Initialization time");
     }
 
     /**
-     * 刷新含有英文的元素
+     * 获取所有的含有英文的文本节点
+     * @param {HTMLElement} root
      */
-    updateElements(){
-        let elements = document.body.querySelectorAll("p,div,a,li,h1,h2,h3");
-        elements = Array.from(elements).filter((element)=>{
-            return element.children.length === 0 && element.innerText!== ""; 
-        })
-        elements=elements.filter((element)=>{
-            return /[a-zA-Z]/.test(element.innerText);
-        })
-        this.elements = elements;
+    getAllTextNodes(element) {
+        const textNodes = [];
+        const walk = document.createTreeWalker(
+            element, 
+            NodeFilter.SHOW_TEXT, // 只遍历文本节点
+            {
+                acceptNode: (node) => {
+                    // 过滤掉空文本节点
+                    if (node.nodeValue.trim() === '') {
+                        return NodeFilter.FILTER_REJECT;
+                    }
+                    // 去除父元素为 <script> 或 <style> 的文本节点
+                    if (node.parentNode.tagName === 'SCRIPT' || node.parentNode.tagName === 'STYLE') {
+                        return NodeFilter.FILTER_REJECT;
+                    }
+                    // 去除已经处理过的元素，也就是有父元素有.ph-span 类的元素
+                    if (node.parentElement.classList.contains('ph-span')) {
+                        return NodeFilter.FILTER_REJECT;
+                    }
+                    // 只留下英文文本节点
+                    if (!/[a-zA-Z]/.test(node.nodeValue)) {
+                        return NodeFilter.FILTER_REJECT;
+                    }
+                    return NodeFilter.FILTER_ACCEPT;
+                }
+            },
+            false
+        );
+    
+        let currentNode;
+        while (currentNode = walk.nextNode()) {
+            textNodes.push(currentNode);
+        }
+        return textNodes;
     }
+
+
+    /**
+     * 在文本节点中使用span包裹单词
+     * @param {TextNode[]} textNodes
+     */
+    wrapWords(textNodes) {
+        for(const textNode of textNodes){
+            //将节点用span包裹
+            const span = document.createElement("span");
+            const str = textNode.nodeValue.trim();
+            //将文本中的单词用span包裹，单词不包含前后的空格，其他文字保持在原位
+            let html = str.replace(/[a-zA-Z]+/g,`<span class="ph-span">$&</span>`);
+            //如果文本末尾是处理的最后一个单词，则要在末尾添加一个空格
+            if(html.endsWith("</span>")){
+                html += "&nbsp;"; 
+            }
+            span.innerHTML = html;
+            //将span插入到文本节点的位置
+            textNode.parentNode.replaceChild(span,textNode);
+        } 
+    }
+
     /**
      * 弹窗显示单词信息
      * @param {HTMLElement}单词所在的元素
@@ -69,66 +120,17 @@ class PhoneticHerper {
     }
     /**
      * 向目标元素添加监听事件
+     * @param {HTMLElement[]} elements 目标元素列表
      */
-    addEventListener(elements){
-        const callback = (event)=>{
-            const text = event.target.innerText.trim();
-            //判断文本是否是单个单词
-            //可以直接解析
-            if(/^[a-zA-Z]+$/.test(text)){
-                this.popup(event.target);
-                return;
-            }
-            //是否有中文
-            //不可直接解析，需要拆分单词重新细分
-            if(/[\u4e00-\u9fa5]/.test(text)){
-                console.log("包含中文");
-                const words = text.match(/[a-zA-Z]+/g);
-                let str = text;
-                for(const word of words){
-                    str = str.replace(word,`<span class="ph-span">${word}</span>`);
-                }
-                event.target.innerHTML = str;
-                event.target.removeEventListener("mouseover",callback);
-                const spans = event.target.querySelectorAll("span");
-                this.addEventListener(spans);
-                return;
-            }
-            //是否没有空格的混合物
-            //不可直接解析，需要拆分单词重新细分
-            if(!/ /.test(text)){
-                const words = text.match(/[a-zA-Z]+/g);
-                if(!words){
-                    event.target.removeEventListener("mouseover",callback);
-                    return;
-                }
-                let str = text;
-                for(const word of words){
-                    str = str.replace(word,`<span class="ph-span">${word}</span>`);
-                }
-                event.target.innerHTML = str;
-                event.target.removeEventListener("mouseover",callback);
-                const spans = event.target.querySelectorAll("span");
-                this.addEventListener(spans);
-                return;
-            }
-
-            //多单词的句子
-            //不可直接解析，需要拆分单词重新细分
-            console.log("多单词的句子");
-            const words = text.split(" ");
-            const html = words.join(`</span> <span class="ph-span">`);
-            event.target.innerHTML = `<span class="ph-span">${html}</span>`;
-            event.target.removeEventListener("mouseover",callback);
-            const spans = event.target.querySelectorAll("span");
-            this.addEventListener(spans);
-        }
-        elements.forEach((element)=>{
-            element.addEventListener("mouseover",callback);
-            element.addEventListener("mouseout",()=>{
+    bindEvents(elements){
+        for(const element of elements){
+            element.addEventListener("mouseenter",()=>{
+                this.popup(element);
+            });
+            element.addEventListener("mouseleave",()=>{
                 this.hidePopup();
-            })
-        }) 
+            });
+        }
     }
     /**
      * 向后台脚本发生单词查询信息
