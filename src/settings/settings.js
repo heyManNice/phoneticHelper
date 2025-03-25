8d4351b4bae1e59bdd07dba3eba4be2a243e2e25
heyManNice <3031978990@qq.com>
2025-03-25 23:10:15 +0800

    feat:添加设置弹出框文件

---
+++ src/settings/settings.js
@@ -0,0 +1,10 @@
+
+class PhoneticHelperSettings{
+    constructor() {
+        window.addEventListener("blur",()=>{
+            //window.close();
+        })
+    }
+}
+
+const settings = new PhoneticHelperSettings();
