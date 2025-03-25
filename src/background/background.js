b9e396a66d63cf9c1817d5fb272d89a393b672fc
heyManNice <3031978990@qq.com>
2025-03-25 14:47:22 +0800

    refactor:background.js

--- src/background/background.js
+++ src/background/background.js
@@ -1,10 +1,25 @@
 import words from "./wordsResource.js";
 
+class PhoneticHelperBackground {
+    constructor(options) {
+        
+    }
+    /**
+     * 查询单词的音标
+     * @param {string} word
+     * @returns {null|{us: string, uk: string}} 音标结果
+     */
+    query(word) {
+        return words[word];
+    }
+}
+const phb = new PhoneticHelperBackground();
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.type) {
         // 处理查询请求
         case "query":
-            sendResponse(words[request.word]);
+            sendResponse(phb.query(request.word));
             break; 
     }
 });
